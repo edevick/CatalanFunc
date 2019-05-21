fa7c303a4bcc23b7f7ce457a95ddbd7c30b97194
edevick <ekdevitskaya@gmail.com>
2019-05-21 12:39:37 +0300

    checked

--- src/CatalanFunction.java
+++ src/CatalanFunction.java
@@ -4,15 +4,15 @@ public class CatalanFunction {
     /* getCatalanNumbers () returns the number of expressions containing n pairs of brackets which are correctly matched
      Catalan formula is C(n) = (2n)!/(n+1)!*n!*/
 
-    public static int getCatalanNumbers(int n){
-          return calculateFactorial(2*n)/(calculateFactorial(n)*calculateFactorial(n+1));
+    public static int getCatalanNumbers(int n) {
+        return calculateFactorial(2 * n) / (calculateFactorial(n) * calculateFactorial(n + 1));
     }
 
-    private static int calculateFactorial(int n){
-        int result = 1;
-        for (int i = 1; i <=n; i ++){
-            result = result*i;
-        }
-        return result;
+    private static int calculateFactorial(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * calculateFactorial(n - 1);
+
     }
 }
